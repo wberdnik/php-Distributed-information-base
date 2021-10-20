4e5f334ba49ce4377e4707d234f8f261f9863a95
Berdnik Vladislav <wberdnik@gmail.com>
2021-10-20 21:15:08 +0300

    Create ExchangeInterface.php

---
+++ interfaces/ExchangeInterface.php
@@ -0,0 +1,54 @@
+<?php
+namespace app\components\DBexchangePlan\interfaces;
+
+/**
+ * Интерфейс отправки и приема пакетов
+ *
+ * @author VLF
+ */
+interface ExchangeInterface {
+
+    /** Регистрация изменений моделей SerializationInterface  Model::afterSave
+     * 
+     * @param bool $insert - запись ActiveRecord в режиме insert
+     * @param SerializationInterface $model
+     */
+    public static function touch(bool $insert, SerializationInterface $model): void;
+    
+    /** Массовая регистрация изменений моделей SerializationInterface  Model::UpdateAll, DeleteAll
+     * 
+     * @param array $condition - отбор по модели
+     * @param string $className
+     */
+    public static function multiTouch(array $condition, string $className): void;
+
+    /** Извлечь массив для отправки
+     * 
+     * @param string $node - consumer
+     * @param int $startCanary - начало интервала канарееечного отбора
+     * @param int $endCanary - начало интервала канарееечного отбора
+     */
+    public static function fetch(string $node,int $startCanary = 0, int $endCanary = 100): ?array;
+
+    /** Применить пакет
+     * 
+     * @param string $node - producer
+     * @param array $pack - данные
+     * @return int Номер квитанции подтверждения
+     */
+    public static function apply(string $node, array $pack): ?int;
+
+    /** Фиксация квитанции пакета 
+     * 
+     * @param string $node - consumer
+     * @param int $ackQueueNumber - подтверждаемая квитанция
+     */
+    public static function acknowledge(string $node, int $ackQueueNumber): void;
+    
+    /** Регистрация ошибки при обмене
+     * 
+     * @param string $text
+     * @return void
+     */
+    public static function addExchError(string $text):void;
+}
