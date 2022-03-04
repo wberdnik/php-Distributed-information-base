558312d66a761f688cd3b59381548922c6c7aea7
Berdnik Vladislav <wberdnik@gmail.com>
2022-03-04 15:19:26 +0300

    Update SerializationInterface.php

--- interfaces/SerializationInterface.php
+++ interfaces/SerializationInterface.php
@@ -1,19 +1,22 @@
 <?php
 namespace app\components\DBexchangePlan\interfaces;
 
-/**
- * Модель, поддерживающая этот интерфейс, автоматически региструется в плане обмена
+/** Модель, поддерживающая этот интерфейс, региструется в плане обмена
  *
  * @author <wberdnik@gmail.com>
  */
 interface SerializationInterface {
-    /**
+    
+    
+    /** Unmutable a entity name
+    *
      * Функция возвращает уникальное имя, которое должно быть таким же для противоположного узла обмена
      * по нему происходит поиск соотвествия таблиц. Должно быть уникальным в пределах одной БД 
      */
     public static function ModelUniversalName(string $node):string;
     
-    /** Запись ActiveRecord с управлением регистрации в плане обмена
+    
+    /** Запись ActiveRecord без регистрации в плане обмена
      * 
      * @param bool $runValidation - валидация
      * @param array $attributeNames list of attribute names that need to be saved. Defaults to null,
@@ -23,14 +26,16 @@ interface SerializationInterface {
      */
     public function pureSave(bool $runValidation = true,?array $attributeNames = null):bool;
     
-    /** Сериализовать объект для ноды
+    
+    /** Сериализовать сущность
      * 
      * @param string $node
      * @return array 
      */
     public function encode(string $node):array;
     
-    /**Фабрика XDTO
+    
+    /** Принять сущность (десерилизовать, проверить наличие, при наличии правил - создать или обновить)
      * 
      * @param string $pack - JSON объект
      * @param string $node - от кого пришел пакет
@@ -38,31 +43,39 @@ interface SerializationInterface {
      */
     public static function decode(array $pack, string $node = ''):?SerializationInterface;
     
-    /**
+    
+    /** Глобальный идентификатор сущности
      * 
      * @return string - универсальная ссылка, из типа (UniversalName) и бизнес ключа
      */
     public function getULink(string $node): string;
     
-    /** Значение бизнес ключа
+    
+    /** Значение бизнес-ключа
      * 
      * @param string $node
      * @return string|null
      */
     public function getBusinesKeyValue(string $node): ?string;
     
-      /**
+    
+    /** Переопредение для управления направленим обмена (есть ли регистрация с этой стороны)
      * @return array of string Список получателей. Если null - все получатели из config
      */
     public static function shallTouchCustomers(): ?array;
     
-    /** Название поля таблицы БД, где хранится бизнес ключ
+    
+    /** Название поля таблицы БД, где хранится бизнес-ключ
      * 
      * @return string - имя атрибута бизнес ключа для участника, Если $node = '', то любой первый
      */
+    
+    
     public static function getColumnBusinesKey(string $node = ''): string;
     
-      /**
+    
+    /** Действие при удалении сущности на другой стороне 
+    *
      * Пришли сведения, что записи удалены  удаленно(remote)
      * Решите сами что с этим делать
      */
